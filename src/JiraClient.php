37024a0a273e61617a6a933b541b9721a63aafe8
Márcio Nóbrega <marcio.nobrega@sapo.pt>
2018-03-10 19:22:16 +0000

    URLencode values.

--- src/JiraClient.php
+++ src/JiraClient.php
@@ -471,9 +471,9 @@ class JiraClient
 
             // some param field(Ex: expand) type is array.
             if (is_array($value)) {
-                $v = implode(',', $value);
+                $v = implode(',', urlencode($value));
             } else {
-                $v = $value;
+                $v = urlencode($value);
             }
 
             $queryParam .= $key.'='.$v.'&';
